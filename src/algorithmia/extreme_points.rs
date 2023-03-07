a0f18987249d463f5111bf684d8e40ebf2c55001
Alberto Natanael Medel Piña <natanael_pi@ciencias.unam.mx>
2023-03-06 20:21:31 -0600

    is_point_inside_triangle first approach-efficient?

--- src/algorithmia/extreme_points.rs
+++ src/algorithmia/extreme_points.rs
@@ -20,45 +20,36 @@ fn _is_point_iside_triangle(p: &Point, q: &Point, r: &Point, curr: &Point) -> bo
     let or2: Direction = orientation(q, r, curr);
     let or3: Direction = orientation(r, p, curr);
 
-
-    let a@(mut or1_c, mut or2_c, mut or3_c) = (false, false, false);
-    let b@(mut or1_cc, mut or2_cc, mut or3_cc) = (false, false, false);
+    let (mut or1_c, mut or2_c, mut or3_c) = (false, false, false);
+    let (mut or1_cc, mut or2_cc, mut or3_cc) = (false, false, false);
 
     match or1 {
-        Direction::Collinear => {or1_c = true}
-        Direction::Clockwise => {}
-        Direction::CounterClockwise => {}
+        Direction::Collinear => {}
+        Direction::Clockwise => {or1_c = true}
+        Direction::CounterClockwise => {or1_cc = true}
         Direction::Invalid => {}
     }
 
     match or2 {
-        Direction::Collinear => {or2_c = true}
-        Direction::Clockwise => {}
-        Direction::CounterClockwise => {}
+        Direction::Collinear => {}
+        Direction::Clockwise => {or2_c = true}
+        Direction::CounterClockwise => {or2_cc = true}
         Direction::Invalid => {}
     }
 
     match or3 {
-        Direction::Collinear => {or2_c = true}
-        Direction::Clockwise => {}
-        Direction::CounterClockwise => {}
+        Direction::Collinear => {}
+        Direction::Clockwise => {or3_c = true}
+        Direction::CounterClockwise => {or3_cc = true}
         Direction::Invalid => {}
     }
-    // let mut or1_c: bool;
-    // let mut or2_c: bool;
-    // let mut or3_c: bool;
 
-    // let mut or1_cc: bool;
-    // let mut or2_cc: bool;
-    // let mut or3_cc: bool;
-    // if (or1 == Direction::Clockwise &&
-    //     or2 == Direction::Clockwise &&
-    //     or3 == Direction::Clockwise){
-    //     return true;
-    // }
+    if or1_c && or2_c && or3_c { return true };
 
+    if or1_cc && or2_cc && or3_cc { return true };
     false
 }
-pub fn _extreme_points() {
 
+pub fn _extreme_points(points: Vec<Point>, n: usize) {
+    let mut convex_hull: Vec<Point> = vec![];
 }
